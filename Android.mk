9d642fed4c628c61fef7aab8ed7bdb6f3758e7b5
yuan-shiguang <385325061@qq.com>
2026-08-22 22:25:17 +0800

    remove gptfdisk

--- Android.mk
+++ Android.mk
@@ -776,7 +776,6 @@ include $(commands_TWRP_local_path)/injecttwrp/Android.mk \
     $(commands_TWRP_local_path)/simg2img/Android.mk \
     $(commands_TWRP_local_path)/adbbu/Android.mk \
     $(commands_TWRP_local_path)/twrpDigest/Android.mk \
-    $(commands_TWRP_local_path)/gptfdisk/Android.mk \
     $(commands_TWRP_local_path)/attr/Android.mk
 
 ifneq ($(TW_OZIP_DECRYPT_KEY),)
