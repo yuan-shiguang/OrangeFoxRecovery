b3c2013d3693234d4b6a4e0bb73ebed7760971b7
yuan-shiguang <385325061@qq.com>
2026-08-24 07:59:03 +0800

    ?

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -565,8 +565,7 @@ endif
 # sgdisk: use the prebuilt statically-linked binary (no .so deps)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sgdisk
-LOCAL_OVERRIDES := sgdisk
-LOCAL_SRC_FILES := sgdisk
+LOCAL_SRC_FILES := sgdisk_static
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
